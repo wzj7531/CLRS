5a5de1bd2cd6dc3569f88a691987aab49afb9fe5
wzj7531 <309910833@qq.com>
2019-04-29 00:21:28 +0800

    daily commit

---
+++ ch04/ex/4.1_5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* find_maxiumu_subarray_Linear (int* A, int length){
+    int left = 0;
+    int right = 0;   
+    int temp_sum = A[0];
+    int max_sum = temp_sum;
+
+    for (int j = 1; j < length; j++){
+        temp_sum += A[j];
+        if(max_sum < temp_sum){
+            right = j;
+            max_sum = temp_sum;
+        }
+        else{
+            if(A[j] > temp_sum){
+                left = j;
+                right = j;
+                temp_sum = A[j];
+                max_sum = temp_sum;
+            }
+        }
+    }    
+
+    int* retArray = (int*)malloc(3 * sizeof(int));
+    retArray[0] = left;
+    retArray[1] = right;
+    retArray[2] = max_sum;
+    return retArray;
+}
+
+int main(){
+    int A[] = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};   
+    int* ret = find_maxiumu_subarray_Linear(A, 15);
+    printf("%d,%d,%d",ret[0],ret[1],ret[2]);
+    return 0;
+}
+--------------------- 
+作者：wzj7531 
+来源：CSDN 
+原文：https://blog.csdn.net/wzj7531/article/details/78254637 
+版权声明：本文为博主原创文章，转载请附上博文链接！
